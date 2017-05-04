7503c75311dbf745f66d6bb5e1823d8c28b3679d
Ola Toft <olatoft@hotmail.com>
2017-05-04 02:45:48 +0200

    Outlies basic skeleton with html and css

--- Main.py
+++ Main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():
+def index():
     return 'hei'
+
+
+@app.route('/draw')
+def draw():
+    return 'this is the draw page'
+
+
+@app.route('/draw/<string:name>/')
+def getPerson(name):
+    return render_template('draw.html', name=name)
 
 if __name__ == '__main__':
     app.run()
