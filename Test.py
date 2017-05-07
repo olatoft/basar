98c5cf17f8c29b792a57f89ca9e8385670c5a384
Ola Toft <olatoft@hotmail.com>
2017-05-07 23:05:11 +0200

    Added another unit test for is_bigger

--- Test.py
+++ Test.py
@@ -8,6 +8,7 @@ class TestForm(unittest.TestCase):
     def test_is_bigger(self):
         self.assertEqual(validation.is_bigger(2, 3), True)
         self.assertEqual(validation.is_bigger(3, 2), False)
+        self.assertEqual(validation.is_bigger(3, 3), False)
 
 if __name__ == '__main__':
     unittest.main()
